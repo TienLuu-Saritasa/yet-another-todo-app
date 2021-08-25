fe412dcd3a945c73347c4172f12be40faf76687b
TienLuu-Saritasa <tien.luu@saritasa.com>
2021-08-25 15:52:13 +0700

    Setup base

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-item.component.scss']
 })
 export class TodoItemComponent implements OnInit {
-
+  isHovered = false;
+  isEditing = false;
   constructor() { }
 
   ngOnInit(): void {
