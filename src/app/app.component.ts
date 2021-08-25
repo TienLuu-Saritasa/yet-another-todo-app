edb5b8f1f7d12834101229cc5b8d0e0c68cf8d96
TienLuu-Saritasa <tien.luu@saritasa.com>
2021-08-25 17:09:43 +0700

    Todo services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { TodoService } from './services/todo.service';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'yet-another-todo-app';
+export class AppComponent implements OnInit {
+  hasTodo$: Observable<boolean> = new BehaviorSubject(true);
+
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit() {
+    this.todoService.fetchFromLocalStorage();
+    this.hasTodo$ = this.todoService.length$.pipe(map((length) => length > 0 ));
+  }
 }
