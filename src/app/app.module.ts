30bf1fe6e3deb05b2999eb2a24b815f25ba4e315
TienLuu-Saritasa <tien.luu@saritasa.com>
2021-08-25 15:18:58 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { TodoInputComponent } from './components/todo-input/todo-input.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    TodoInputComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
